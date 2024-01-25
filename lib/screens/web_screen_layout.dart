4df995373ffd5e2b356347042c3f53c91e535ce1
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-01-25 19:44:56 +0530

    Added Web Search Bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/contacts_list.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/web_profile_bar.dart';
+import 'package:whatsapp_ui_clone_flutter/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -16,7 +17,7 @@ class WebScreenLayout extends StatelessWidget {
               child: Column(
                 children: [
                   WebProfileBar(),
-                  //   Web Search Bar
+                  WebSearchBar(),
                   ContactsList(),
                 ],
               ),
---
+++ lib/widgets/web_search_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone_flutter/colors.dart';
+
+class WebSearchBar extends StatelessWidget {
+  const WebSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.06,
+      width: MediaQuery.of(context).size.width * 0.25,
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: dividerColor),
+        ),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: searchBarColor,
+            prefixIcon: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(Icons.search),
+            ),
+            hintStyle: const TextStyle(fontSize: 14),
+            hintText: 'Search or Start a new chat',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(
+                width: 0,
+                style: BorderStyle.none,
+              ),
+            ),
+            contentPadding: EdgeInsets.all(10)),
+      ),
+    );
+  }
+}
