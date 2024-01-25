ca4a3682b5f0d19b594a8aaafd29a2ff97c5a6a5
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-01-25 22:35:11 +0530

    Web Chat Appbar added

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/contacts_list.dart';
+import 'package:whatsapp_ui_clone_flutter/widgets/web_chat_appbar.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/web_profile_bar.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/web_search_bar.dart';
 
@@ -30,6 +31,11 @@ class WebScreenLayout extends StatelessWidget {
                   image: AssetImage('assets/backgroundImage.png'),
                   fit: BoxFit.cover),
             ),
+            child: Column(children: [
+              WebChatAppBar(),
+              //   Chat List
+              //   Messsage Input box
+            ]),
           )
         ],
       ),
