97ccb6c3506591f0bed7bd69e2ff8402154183da
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-01-25 17:44:12 +0530

    Web Contacts List added

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -8,15 +8,14 @@ class ContactsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: info.length,
-                itemBuilder: (context, index) {
-                  return InkWell(
+        padding: EdgeInsets.only(top: 10),
+        child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: info.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  InkWell(
                     onTap: () {},
                     child: Padding(
                       padding: const EdgeInsets.only(bottom: 8.0),
@@ -35,20 +34,19 @@ class ContactsList extends StatelessWidget {
                         leading: CircleAvatar(
                           backgroundImage: NetworkImage(
                               info[index]['profilePic'].toString()),
+                          radius: 30,
                         ),
                         trailing: Text(info[index]['time'].toString(),
                             style: TextStyle(fontSize: 13, color: Colors.grey)),
                       ),
                     ),
-                  );
-                }),
-          ),
-          const Divider(
-            color: dividerColor,
-            indent: 85,
-          )
-        ],
-      ),
-    );
+                  ),
+                  const Divider(
+                    color: dividerColor,
+                    indent: 85,
+                  )
+                ],
+              );
+            }));
   }
 }
