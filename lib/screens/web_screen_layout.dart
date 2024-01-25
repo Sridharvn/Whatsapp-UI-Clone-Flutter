458352792ea236af17e7d96a8511e324a3d304bb
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-01-25 23:12:31 +0530

    Added Web Input message field

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone_flutter/colors.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/chat_list.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/contacts_list.dart';
 import 'package:whatsapp_ui_clone_flutter/widgets/web_chat_appbar.dart';
@@ -35,7 +36,55 @@ class WebScreenLayout extends StatelessWidget {
             child: Column(children: [
               WebChatAppBar(),
               Expanded(child: ChatList()),
-              //   Messsage Input box
+              Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: dividerColor),
+                  ),
+                  color: chatBarMessage,
+                ),
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.emoji_emotions_outlined,
+                          color: Colors.grey,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.attach_file,
+                          color: Colors.grey,
+                        )),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10, right: 15),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              fillColor: searchBarColor,
+                              filled: true,
+                              hintText: 'Enter Message Here...',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(
+                                    width: 0, style: BorderStyle.none),
+                              ),
+                              contentPadding: EdgeInsets.only(left: 20)),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.mic,
+                          color: Colors.grey,
+                        )),
+                  ],
+                ),
+              )
             ]),
           )
         ],
